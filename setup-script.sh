e9fd1a8c2f1e689989ff4545d09e467b0e5691c8
BicycleWalrus <32198667+BicycleWalrus@users.noreply.github.com>
2025-01-09 11:34:28 -0500

    Create setup-script.sh

---
+++ setup-script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check if kubectl is installed
+if ! command -v kubectl &> /dev/null
+then
+    echo "Error: kubectl is not installed. Please install it first."
+    exit 1
+fi
+
+# Applying Lab Pre-Requisites
+echo "Lab pre-requisites..."
+if kubectl apply -f app.yaml; then
+    echo "Successfully applied pre-requisites..."
+else
+    echo "Error: Failed to apply app.yaml"
+    exit 1
+fi
+
+# Applying the Vulnerable Workload: Pillow
+echo "Applying the vulnerable workload: pillow"
+if kubectl apply -f pillow.yaml; then
+    echo "Successfully applied the vulnerable workload: pillow"
+else
+    echo "Error: Failed to apply pillow.yaml"
+    exit 1
+fi
+
+echo "Setup Complete!"
